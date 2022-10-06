5efde2abe960dd98365f49f7eaf21791a28a398c
Jacopo Vitale <jacopo.vitale96@gmail.com>
2022-10-06 11:52:28 +0200

    Delete pixelpilot/build directory

--- pixelpilot/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jacopo/catkin_ws/src/pixelpilot
-# Build directory: /home/jacopo/catkin_ws/src/pixelpilot/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
